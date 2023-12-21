d5bca21a5c38013fdaadb57b1068f4ff4b0d2731
Uday Bhardwaj <udaybh07@gmail.com>
2023-12-21 15:03:16 +0530

    All other functionalities are working except downloading

--- Video_download/scr.py
+++ Video_download/scr.py
@@ -1,21 +1,22 @@
 from pytube import YouTube
 import sys
+from prettytable import PrettyTable
+from time import sleep
 
 
-# testing video id
-# kOYcbod5J0w
-# RuYC6U3LBRs
-
-class VIDEO():
+class Video():
   
   def __init__(self,id:str):
     """
     This gives a better approaach to getting the info regarding the video and download it from different streams.
     : `id`  It is the youtube video id
-    
     """
+    
+    # Setting up the general information and obtaining the video. 
     self.link = "https://www.youtube.com/"+ id
     self.clip = YouTube( self.link )
+      
+    sleep(0.2)
     
     # General information about the video
     self.name = self.clip.title
@@ -25,61 +26,136 @@ class VIDEO():
     self.audio = self.clip.streams.filter( only_audio = True )
     self.video = self.clip.streams.filter( only_video = True )
     self.normal = self.clip.streams.filter( progressive = True )
+
+  def options(self):
+      """This just maps the options given to the given functions."""
+
+      dt_opt = {
+          'title' : self.title , 'thumbnail' : self.thumbnail ,  'audio' : self.stream_audio , 'video' : self.stream_video ,  'normal': self.stream_normal , 'stop': self.stop , 'download' : self.download , 'options' : self.display_options
+        }
+
+      self.display_options()
+      checker1=1
+
+      while( checker1 ):
+
+        print("\n")
+        opt = self.input_comp()
+        self.line()
+
+        if opt not in dt_opt.keys():
+          print( "\nSorry, the option which you have entered is incorrect. Try again" )
+        else:
+          ans=dt_opt[opt]()     # Calling function through the mapping
+          checker1 = ans
+
+      print("\nExiting the script for the current video id...........................Done :)\n")
+
+  def display_options(self):
+    """This displays different options which can used with the script."""
+    dt = PrettyTable( ['Option' , 'Function'] )
+    dt.align='c'
     
+    dt.add_rows([
+      [ 'title' , 'To show the title of the video' ],
+      [ 'thumbnail' , 'It gives a link to the video\'s thummbnail' ],
+      [ 'video' , 'To show properties of the only video format files' ],
+      [ 'audio' , 'To show properties of the only audio format files' ],
+      [ 'normal' , 'To show properties of both audio and video format files' ],
+      [ 'download' , 'To download the video with the given tag.' ],
+      [ 'stop' , 'To quit the options for the current video' ],
+      ['options' , 'To display options']
+    ] )
+    
+    print("\nThere are different options which you can try. The list of options is given below.")
+    print( dt )
+    
+    return 1
+
+  def input_comp( self )->str:
+    """It is used to counter some of the errors which can be encountered during the entering of the options or the download link."""
+    
+    try:
+      inp = input("Enter the option: ")
+      return inp
+    except KeyboardInterrupt:
+      print( "\nYou have interrupted the program. Run the script again to get information about downloading the video" )
+      self.stop()
+    except EOFError:
+      print( "Please enter something in the options bar." )
+
   def title( self ):
-    print( self.name )
+    """This shows the title of the video"""
     
+    print( self.name )
+    return 1
+  
   def thumbnail( self ):
-    print( self.image )  
+    """This gives the link to the thumbnail file"""
+        
+    print( self.image )
+    return 1
 
   def stream_audio( self ):
+    """This prints the available options for audio streams."""
     
-    for id in stream:
-      print(id,end="\n")
+    dt=PrettyTable(  ['Tag'  , 'Type' , 'Average bit rate (abr)' , 'acodec']  )
+    dt.align='c'
+    
+    for elm in self.audio:
+      dt.add_row( [ elm.itag  , elm.type , elm.abr , elm.audio_codec ]  )
+    
+    print( "\n" , dt )
+    
+    return 1
 
-  def stream_video(samp):
-    stream  = samp.streams.filter(only_video=True)
-    for id in stream:
-      print(id,end="\n")
+  def stream_video(self):
+    """This prints the available options for video streams and their properties."""
+    
+    dt = PrettyTable(  ['Tag' , 'Type' , 'Resolution' , 'Video Codec']  )
+    dt.align='c'
+    
+    for elm in self.video:
+      dt.add_row( [ elm.itag , elm.type , elm.resolution , elm.video_codec ]  )
 
-  def NORMAL(samp):
-    stream  = samp.streams.filter(progressive=True)
-    for id in stream:
-      print(id,end="\n")
+    print( "\n" , dt )
+    
+    return 1
 
-  def DOWNLOAD(samp):
+  def stream_normal( self ):
+    """This displays the options for normal video format."""
+    
+    dt=PrettyTable(  ['Tag' , 'Type' , 'Frames/sec' , 'Resolution' , 'Video Codec' , 'Audio Codec' ]  )
+    dt.align='c'
+    
+    for elm in self.normal:
+      dt.add_row( [ elm.itag , 'Normal' , elm.fps , elm.resolution , elm.video_codec , elm.audio_codec ] )
+      
+    print( "\n" , dt )
+    
+    return 1
+
+  def download( self ):
     tag=int(  input("\nEnter the tag number which you have taken by finding the progressive, audio and video streams\n")  )
     vd=samp.streams.get_by_itag(tag)
 
     vd.download(output_path='Videos/yt')
     print("Finished with the download")
 
-  def options(chc,samp)->int:
-    """This just maps the options given to the given functions."""
-
-    if chc==0:
-      print(TITLE(samp))
-
-    elif chc==1:
-      THUMBNAIL(samp)
-    elif chc==2:
-      AUDIO_STREAM(samp)
-    elif chc==3:
-      VIDEO_STREAM(samp)
-    elif chc==4:
-      NORMAL(samp)
-    elif chc==5:
-      DOWNLOAD(samp)
-      print("Download Finished....")
-
-    elif chc==6:
-      print("\nFinishing with the current video.......\n---------------------------------------------------------------------------------------------------------------------------------------------------")
-      return 0
-
     return 1
 
+  def line(self):
+    """It simply provides an ASCII line separator."""
+    
+    print("\n---------------------------------------------------------------------------------------------------", end="\n" )
 
-  # The Main Script
+  def stop(self)->int:
+    """It stops the program from running further."""
+    
+    return 0
+
+
+# The Main Script
 def main():
   """It takes in the id to the youtube video and returns some other information regarding to it."""
 
@@ -89,42 +165,21 @@ def main():
 
   else:
     # Creating the link for the video
-    print("---------------------------------------------------------------------------------------------------------------------------------------------------")
-    VIDEO()
+        
     for i in range(1,num_arg):
       
       print(f"Checking for the video with the id number {i}............\n")
-      inp=sys.argv[i]
-      LINK="https://www.youtube.com/"+ inp
+      ID = sys.argv[i]
 
       try:
-        video = yt(LINK)
+        vid = Video( ID )
       except:
-        print("\nThe video id is not valid or there is some other error which is encountered.\n")
+        raise Exception( "Sorry, An error has occured. Please check the video id or the video might not be available. Proceeding on to the next video." )
         continue
+      
+      print( f"Video:  {vid.name}." )
+      vid.options()
 
-      print(f"Video:  {TITLE(video)}.\nThere are various options which you can enter and try:\n\n'title'   To get the title of the video.\n'thumb'  To get the thumbnail of the video.\n'video'  To get the options for only-video downloading.\n'audio'  To get options for only-audio downloading.\n'normal'  To get options for a file containing both audio and video.\n'download'  To download the video\n'stop'  To stop the processing for the current video.")
-      dt={
-        'title':0,  'thumb':1,  'audio':2,  'video':3,  'normal':4, 'stop':6, 'download':5
-      }
-
-      per=1
-      c1=0
-      while(per):
-        opt=input("\nPlease Enter your option: ").lower()
-
-        try:
-          valid=options(dt[opt],video)
-        except:
-          print("Sorry, You have entered invalid command.\n")
-          c1+=1
-          if c1==5:
-            per=0
-        finally:
-          try: per=valid; 
-          except: per=0
-
-  print("\nExiting the script...........................Done :)")
 
 # Running the script
 if __name__ == "__main__":
